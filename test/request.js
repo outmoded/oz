4a7eeee37f0f6963341dcb344250d6820683a31f
Eran Hammer <eran@hueniverse.com>
2012-09-22 19:48:42 -0700

    Add app id to ticket and header mac

--- test/request.js
+++ test/request.js
@@ -10,10 +10,13 @@ describe('oz', function () {
 
         it('should construct and parse a valid authentication header', function (done) {
 
+            // Note: the ticket.id already encodes all the other ticket attributes and they cannot be manually changed
+            
             var ticket = {
                 id: '4deee737c1810925ace5aa5292c4e761f2325eb1286bc5c69cbf00b3f5de3abc:eL5Zvd2wyIiMc-6Adk2SUy7i4TjZKLnV_KTUYnTri5Q:a5f7aa17320716247dd18fd87f04e7c0495980b3417d94185f0feb6c052e123e:p1BY4SLSY-5fjKuPSz_GwQ:UDPFp5jLSyYZmGrlD111XxNrZzhvWdU32k_05EjPm4vi0pynvYpGGXYTuuxlEj7hwUR4BOmFumASxvZJVRMMERhCtOjqBwUbU9L8MzI2wYYEryFImSwDkxZAamsG37KH6K1w-rTP-UgP8mVpmboA9-vzwRrlaPzvV19VS7kLGEUeDR8DFzwQpMl2lK-dw4KQPPmsKSGFzxlUO-9hpvWdU6lyTdMYAoy8MPTNCMT4NbgRrjitYV-6YKmhJNHMErzs',
                 key: '1a8da59012b3aa2100b900ccef0b6dbd574350e962dd8c176bfddb70aec75cb0',
-                algorithm: 'sha256'
+                algorithm: 'sha256',
+                app: '123'
             };
               
             var request = {
@@ -29,7 +32,7 @@ describe('oz', function () {
             };
 
             var mac = Oz.Request.mac(request, attributes, ticket);
-            var header = Oz.Request.formatHeader(attributes, ticket.id, mac);
+            var header = Oz.Request.formatHeader(attributes, ticket.id, ticket.app, mac);
 
             var req = {
                 method: request.method,
