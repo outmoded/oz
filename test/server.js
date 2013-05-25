f507fefd0ea03ea2aabf57c60d199655a1bc74dc
Eran Hammer <eran@hueniverse.com>
2013-05-25 16:46:51 -0700

    100% server coverage

--- test/server.js
+++ test/server.js
@@ -1,6 +1,7 @@
 // Load modules
 
 var Lab = require('lab');
+var Hawk = require('hawk');
 var Oz = require('../lib');
 
 
@@ -29,6 +30,168 @@ describe('Server', function () {
                 expect(err).to.exist;
                 expect(err.message).to.equal('Invalid encryption password');
                 done();
+            });
+        });
+
+        var encryptionPassword = 'welcome!';
+
+        var app = {
+            id: '123'
+        };
+
+        it('authenticates a request', function (done) {
+
+            var grant = {
+                id: 's81u29n1812',
+                user: '456',
+                exp: Hawk.utils.now() + 5000,
+                scope: ['a', 'b']
+            };
+
+            Oz.ticket.issue(app, grant, encryptionPassword, {}, function (err, envelope) {
+
+                expect(err).to.not.exist;
+
+                var req = {
+                    method: 'POST',
+                    url: '/oz/rsvp',
+                    headers: {
+                        host: 'example.com',
+                        authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', envelope).field
+                    }
+                };
+
+                Oz.server.authenticate(req, encryptionPassword, {}, function (err, ticket, ext) {
+
+                    expect(err).to.not.exist;
+                    done();
+                });
+            });
+        });
+
+        it('fails to authenticate a request with bad password', function (done) {
+
+            var grant = {
+                id: 's81u29n1812',
+                user: '456',
+                exp: Hawk.utils.now() + 5000,
+                scope: ['a', 'b']
+            };
+
+            Oz.ticket.issue(app, grant, encryptionPassword, {}, function (err, envelope) {
+
+                expect(err).to.not.exist;
+
+                var req = {
+                    method: 'POST',
+                    url: '/oz/rsvp',
+                    headers: {
+                        host: 'example.com',
+                        authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', envelope).field
+                    }
+                };
+
+                Oz.server.authenticate(req, 'x', {}, function (err, ticket, ext) {
+
+                    expect(err).to.exist;
+                    expect(err.message).to.equal('Bad hmac value');
+                    done();
+                });
+            });
+        });
+
+        it('fails to authenticate a request with expired ticket', function (done) {
+
+            var grant = {
+                id: 's81u29n1812',
+                user: '456',
+                exp: Hawk.utils.now() - 5000,
+                scope: ['a', 'b']
+            };
+
+            Oz.ticket.issue(app, grant, encryptionPassword, {}, function (err, envelope) {
+
+                expect(err).to.not.exist;
+
+                var req = {
+                    method: 'POST',
+                    url: '/oz/rsvp',
+                    headers: {
+                        host: 'example.com',
+                        authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', envelope).field
+                    }
+                };
+
+                Oz.server.authenticate(req, encryptionPassword, {}, function (err, ticket, ext) {
+
+                    expect(err).to.exist;
+                    expect(err.message).to.equal('Expired ticket');
+                    done();
+                });
+            });
+        });
+
+        it('fails to authenticate a request with mismatching app id', function (done) {
+
+            var grant = {
+                id: 's81u29n1812',
+                user: '456',
+                exp: Hawk.utils.now() + 5000,
+                scope: ['a', 'b']
+            };
+
+            Oz.ticket.issue(app, grant, encryptionPassword, {}, function (err, envelope) {
+
+                expect(err).to.not.exist;
+
+                envelope.app = '567';
+                var req = {
+                    method: 'POST',
+                    url: '/oz/rsvp',
+                    headers: {
+                        host: 'example.com',
+                        authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', envelope).field
+                    }
+                };
+
+                Oz.server.authenticate(req, encryptionPassword, {}, function (err, ticket, ext) {
+
+                    expect(err).to.exist;
+                    expect(err.message).to.equal('Mismatching application id');
+                    done();
+                });
+            });
+        });
+
+        it('fails to authenticate a request with mismatching dlg id', function (done) {
+
+            var grant = {
+                id: 's81u29n1812',
+                user: '456',
+                exp: Hawk.utils.now() + 5000,
+                scope: ['a', 'b']
+            };
+
+            Oz.ticket.issue(app, grant, encryptionPassword, {}, function (err, envelope) {
+
+                expect(err).to.not.exist;
+
+                envelope.dlg = '567';
+                var req = {
+                    method: 'POST',
+                    url: '/oz/rsvp',
+                    headers: {
+                        host: 'example.com',
+                        authorization: Oz.client.header('http://example.com/oz/rsvp', 'POST', envelope).field
+                    }
+                };
+
+                Oz.server.authenticate(req, encryptionPassword, {}, function (err, ticket, ext) {
+
+                    expect(err).to.exist;
+                    expect(err.message).to.equal('Mismatching delegated application id');
+                    done();
+                });
             });
         });
     });
